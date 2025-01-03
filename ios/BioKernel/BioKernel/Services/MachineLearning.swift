3a276b29f12ec1d807504aa19ffe6d806e27ee94
Sam King <kingst@gmail.com>
2025-01-02 19:44:25 -0800

    Add settings for ML gain

--- ios/BioKernel/BioKernel/Services/MachineLearning.swift
+++ ios/BioKernel/BioKernel/Services/MachineLearning.swift
@@ -44,12 +44,12 @@ struct MLUtilities {
     }
 }
 
-actor LocalAIDosing: MachineLearning {
-    static let shared = LocalAIDosing()
-    let aiDosingGain = 2.0
+actor AIDosing: MachineLearning {
+    static let shared = AIDosing()
     
     func tempBasal(settings: CodableSettings, glucoseInMgDl: Double, targetGlucoseInMgDl: Double, insulinOnBoard: Double, dataFrame: [AddedGlucoseDataRow]?, at: Date) async -> Double? {
         
+        print("AI temp basal")
         // make sure we have enough data
         guard let dataFrame = dataFrame else { return nil }
 
@@ -57,22 +57,27 @@ actor LocalAIDosing: MachineLearning {
         let hour = Calendar.current.component(.hour, from: at)
         guard hour < 22 && hour > 8 else { return nil }
         
+        print("AI temp basal hour: (\(hour)), checking glucose rising")
         // make sure that glucose is rising
         guard let predicted = await getPhysiologicalModels().predictGlucoseIn15Minutes(from: at) else { return nil }
-        guard glucoseInMgDl >= 180, predicted > glucoseInMgDl else { return nil }
+        guard predicted >= 180, predicted > glucoseInMgDl else { return nil }
         
-        // calculate added glucose
+        print("AI temp basal final calcs")
+        // calculate added glucose and dose
         let insulinSensitivity = settings.learnedInsulinSensitivity(at: at)
         guard let addedGlucose = dataFrame.addedGlucosePerHour30m(insulinSensitivity: insulinSensitivity) else { return nil }
-        
-        // ok, let's go ahead and dose
         let insulinNeeded = addedGlucose / insulinSensitivity - insulinOnBoard
-        return aiDosingGain * insulinNeeded  * 1.hoursToSeconds() / settings.correctionDurationInSeconds
+
+        let aiDosingGain = settings.getMachineLearningGain()
+        let dose = aiDosingGain * insulinNeeded  * 1.hoursToSeconds() / settings.correctionDurationInSeconds
+        print("AI temp basal @ \(dose) for 30m")
+        
+        return dose
     }
 }
 
-actor LocalMachineLearning: MachineLearning {
-    static let shared = LocalMachineLearning()
+actor DNNDosing: MachineLearning {
+    static let shared = DNNDosing()
     
     // our current prediction uses an ML model to predict addedGlucose
     // then runs it through the same calculations that we use for
