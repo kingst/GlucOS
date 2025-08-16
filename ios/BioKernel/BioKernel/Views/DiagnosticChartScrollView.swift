e0e8abf4762cbb75d82a80251113e1aabf908434
Sam King <kingst@gmail.com>
2025-08-16 11:40:38 -0700

    Reviewer feedback

--- ios/BioKernel/BioKernel/Views/DiagnosticChartScrollView.swift
+++ ios/BioKernel/BioKernel/Views/DiagnosticChartScrollView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct DiagnosticChartScrollView<Content: View>: View {
     @Binding var selectedHours: Int
     let content: Content
-
+    let scrollAndAnchorId = "scroll_end_anchor"
+    
     init(selectedHours: Binding<Int>, @ViewBuilder content: () -> Content) {
         self._selectedHours = selectedHours
         self.content = content()
@@ -20,20 +21,22 @@ struct DiagnosticChartScrollView<Content: View>: View {
         GeometryReader { geometry in
             ScrollViewReader { proxy in
                 ScrollView(.horizontal, showsIndicators: false) {
+                    // avoid divide by 0
+                    let hours = selectedHours != 0 ? selectedHours : 4
                     HStack(spacing: 0) {
                         content
                             .padding()
-                            .frame(width: geometry.size.width * (selectedHours > 0 ? CGFloat(24 / selectedHours) : 1.0))
+                            .frame(width: geometry.size.width * CGFloat(24 / hours))
                         
-                        Color.clear.frame(width: 0, height: 0).id("scroll_end_anchor")
+                        Color.clear.frame(width: 0, height: 0).id(scrollAndAnchorId)
                     }
                 }
                 .onAppear {
-                    proxy.scrollTo("scroll_end_anchor", anchor: .trailing)
+                    proxy.scrollTo(scrollAndAnchorId, anchor: .trailing)
                 }
                 .onChange(of: selectedHours) { _ in
                     withAnimation {
-                        proxy.scrollTo("scroll_end_anchor", anchor: .trailing)
+                        proxy.scrollTo(scrollAndAnchorId, anchor: .trailing)
                     }
                 }
             }
